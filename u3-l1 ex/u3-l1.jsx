7aae2db0eaad49b6ad9710ad66be98fd059d6c6c
GabrielaBianca04 <143381847+GabrielaBianca04@users.noreply.github.com>
2023-10-23 17:10:31 +0200

    Add files via upload

---
+++ u3-l1 ex/u3-l1.jsx
@@ -0,0 +1,15 @@
+function ButtonComponent (){
+    return (
+        <button className="button">Click here</button>
+    )
+}
+
+export default ButtonComponent;
+
+class ImageComponent extends React.Component {
+  render(){
+    return(
+      <img src="" alt=""></img>
+    )
+  }
+}
